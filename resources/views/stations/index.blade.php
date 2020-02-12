b51eacc0d9c5ccae3c295226dc4a0f2a890a1fa6
fjelder <mich.filipiak@gmail.com>
2020-02-12 19:58:41 +0100

    Change template

--- resources/views/stations/index.blade.php
+++ resources/views/stations/index.blade.php
@@ -3,36 +3,34 @@
 @section('title', 'Lista stacji')
 @section('card_title', 'Lista stacji')
 @section('content')
-<div class="row">
-  <div class="col-md-6">
-    <div class="table-responsive">
-      <table class="table">
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">Nazwa</th>
-            <th scope="col">Akcja</th>
-          </tr>
-        </thead>
-        <tbody>
-          @foreach ($stations as $key => $station)
-          <tr>
-            <th scope="row">{{$key+1}}</th>
-            <td><a href="{{route('stations.show', $station->id)}}">{{$station->name}}</a></td>
-            <td>
-              <a href="{{route('stations.edit', $station->id)}}" class="btn btn-outline-primary" role="button">Edytuj</a>
-              <form action="{{ route('stations.destroy', $station->id) }}" method="POST">
-   	 						@csrf
-								@method('delete')
-								<button type="submit" class="btn btn-primary">Kasuj</button>
-							</form>
-            </td>
-          </tr>
-          @endforeach
-        </tbody>
-      </table>
-    </div>
-  </div>
+<div class="table-responsive">
+  <table class="table">
+    <thead class=" text-primary">
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Nazwa</th>
+        <th scope="col">Komentarz</th>
+        <th scope="col">Akcja</th>
+      </tr>
+    </thead>
+    <tbody>
+      @foreach ($stations as $key => $station)
+      <tr>
+        <th scope="row">{{$key+1}}</th>
+        <td><a href="{{route('stations.show', $station->id)}}">{{$station->name}}</a></td>
+        <td>{{$station->comments}}</td>
+        <td>
+            <form action="{{ route('stations.destroy', $station->id) }}" method="POST">
+            @csrf
+            @method('delete')
+            <a href="{{route('stations.edit', $station->id)}}" class="btn btn-outline-primary" role="button">Edytuj</a>
+            <button class="btn btn-outline-danger" type="submit">Usuń</button>
+          </form>
+        </td>
+      </tr>
+      @endforeach
+    </tbody>
+  </table>
 </div>
 <a href="{{route('stations.create')}}">Dodaj nową stację</a>
 @endsection
