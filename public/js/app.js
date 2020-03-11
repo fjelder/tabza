e474fbcd25bb5354cdaa821632357b8abf548ebd
fjelder <mich.filipiak@gmail.com>
2020-03-11 17:27:35 +0100

    change the railroads form

--- public/js/app.js
+++ public/js/app.js
@@ -1,39 +1,74 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-	"use strict";
+// 	"use strict";
 
-	var max_fields = 9; //maximum input boxes allowed
-	var wrapper = $(".input_fields_wrap"); //Fields wrapper
-	var add_button = $(".add_field_button"); //Add button ID
+// 	var max_fields = 9; //maximum input boxes allowed
+// 	var wrapper = $(".input_fields_wrap"); //Fields wrapper
+// 	var add_button = $(".add_field_button"); //Add button ID
 
-	var x = 1; //initlal text box count
-	$(add_button).click(function(e) { //on add input button click
-		e.preventDefault();
-		if (x <= max_fields) { //max input box allowed
-			x++; //text box increment
-			$(wrapper).append('<div class="form-group"><div class="form-row"><div class="col-2"><label class="col-form-label xx2">#' + x + '</label></div><div class="col-7"><input type="text" name="name[]" placeholder="..." class="form-control"/></div><div class="col-3"><a href="#" class="remove_field btn"><i class="far fa-trash-alt"></i></a></div></div></div>'); //add input box
+// 	var x = 1; //initlal text box count
+// 	$(add_button).click(function(e) { //on add input button click
+// 		e.preventDefault();
+// 		if (x <= max_fields) { //max input box allowed
+// 			x++; //text box increment
+// 			$(wrapper).append('<div class="form-group"><div class="form-row"><div class="col-2"><label class="col-form-label xx2">#' + x + '</label></div><div class="col-7"><input type="text" name="name[]" placeholder="..." class="form-control"/></div><div class="col-3"><a href="#" class="remove_field btn"><i class="far fa-trash-alt"></i></a></div></div></div>'); //add input box
+// 		}
+// 	});
+
+// 	$(wrapper).on("click", ".remove_field", function(e) { //user click on remove text
+// 		e.preventDefault();
+// 		$(this).parentsUntil(wrapper).remove();
+// 		x--;
+// 		$('input[name="name[]"]').each(function(index) {
+// 			$(this).val(index + 1);
+// 		});
+// 		$('.xx2').each(function(index) {
+// 			$(this).empty();
+// 			$(this).append('#');
+// 			$(this).append(index + 1);
+// 		});
+// 	});
+
+	// 	var rows = $("#cRow").val();
+	// 	var container = $('.addInput');
+	// 	// container.empty();
+	// 	for (var i = 0; i < rows; i++) {
+	// 		container.append('<div class="col-4 col-lg-2"><label for="" class="col-form-label">#'+(i+1)+'</label><input type="text" class="form-control"></div>');
+	// 	}
+
+
+	// 	Nowa część kodu
+	var addRow = $('.addRow');
+	var cntRow = $('#cntRow').val(); //aktualna liczba wierszy
+
+	//y - wartość początkowa, x - ilość wierszy, name - nazwa inputów, val - wartość
+	function addRows(y, x, name, value = '') {
+		for (i = 0; i < x; i++) {
+			addRow.append('<div class="form-group col-sm-2"><label for="" class="col-form-label col-form-label-sm">#' + (i + y) + '</label><input name="' + name + (i + y) + '" type="text" class="form-control form-control-sm" value="' + value + '"></div>');
+		}
+	}
+	//addRows(1, cntRow, 'zwr', '#');
+
+// 	$("#cntRow").on('change', function() {
+// 		alert('test');
+// 	});
+	$("#cntRow").on('change', function() {
+		if ($('#cntRow').val() > cntRow) {
+			ilosc = $('#cntRow').val() - cntRow; //ile dodajemy wierszy
+			//initRow = parseInt(cntRow) + 1;
+			xx = parseInt(($('.addRow .form-group:last-child :input').attr("name")).substr(3)) + 1;
+			addRows(xx, ilosc, 'zwr', ''); //generujemy nowe wiersze
+			cntRow = $('#cntRow').val(); //zmiana wartosci aktualnej liczby wierszy
+		} else {
+			ilosc = cntRow - $('#cntRow').val(); //ile wierszy usuwamy
+			for (i = ilosc; i > 0; i--) {
+				$("input[name='zwr" + (parseInt($('#cntRow').val()) + i) + "']").parent().remove();
+			}
+			cntRow = $('#cntRow').val(); //zmiana wartosci aktualnej liczby wierszy
 		}
 	});
-
-	$(wrapper).on("click", ".remove_field", function(e) { //user click on remove text
-		e.preventDefault();
-		$(this).parentsUntil(wrapper).remove();
-		x--;
-		$('input[name="name[]"]').each(function(index) {
-			$(this).val(index + 1);
-		});
-		$('.xx2').each(function(index) {
-			$(this).empty();
-			$(this).append('#');
-			$(this).append(index+1);
-		});
+	$('.x').click(function() {
+		xx = $('.addRow .form-group:last-child :input').attr("name");
+		addRow.append(xx.substr(3));
 	});
-
-	var rows = $("#cRow").val();
-	var container = $('.addInput');
-	// container.empty();
-	for (var i = 0; i < rows; i++) {
-		container.append('<div class="col-4 col-lg-2"><label for="" class="col-form-label">#'+(i+1)+'</label><input type="text" class="form-control"></div>');
-	};
-});  
-
+})
--- resources/views/railroads/index.blade.php
+++ resources/views/railroads/index.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.app') @section('title', 'Lista zwrotnic') @section('content')
 <div class="row">
-	<div class="col-12">
+	<div class="col-12 mt-3 p-3 shadow">
 		<h5 class="m-0">Istniejące zwrotnice stacji <span style="color:#0a829a;">{{ $station_name }}</span></h5>
 				<p class="card-title float-none"><a href="{{ route('stations.show', $station_id)}}">Wróć do szczegółów stacji</a></p>
-		<div class="form-group row">
+<!-- 		<div class="form-group row">
 			<div class="col-4 col-lg-1">
 				<label for="cRow" class="col-form-label d-inline">Ilość wierszy</label>
 				<select name="cRow" id="cRow" class="form-control d-inline">
@@ -17,6 +17,70 @@
 		</div>
 		<div class="row addInput">
 		</div><!-- addInput -->	
+				
+		<form action="" method="post">
+    <div class="form-row">
+      <div class="form-group col-sm-2">
+        <label for="inputState">Ilość rozjazdów</label>
+        <select id="cntRow" class="form-control form-control-sm">
+          <option value="10" selected>10</option>
+          <option value="20">20</option>
+          <option value="30">30</option>
+          <option value="40">40</option>
+          <option value="50">50</option>
+        </select>
+      </div>
+    </div>
+    <div class="form-row addRow">
+      <div class="form-group col-sm-2">
+        <label for="" class="col-form-label col-form-label-sm">#1</label>
+        <input name="zwr1" type="text" class="form-control form-control-sm mb-3" value="1/2">
+      </div>
+      <div class="form-group col-sm-2">
+        <label for="" class="col-form-label col-form-label-sm">#2</label>
+        <input name="zwr2" type="text" class="form-control form-control-sm" value="3/4">
+      </div>
+      <div class="form-group col-sm-2">
+        <label for="" class="col-form-label col-form-label-sm">#3</label>
+        <input name="zwr3" type="text" class="form-control form-control-sm" value="5">
+      </div>
+      <div class="form-group col-sm-2">
+        <label for="" class="col-form-label col-form-label-sm">#4</label>
+        <input name="zwr4" type="text" class="form-control form-control-sm" value="6">
+      </div>
+      <div class="form-group col-sm-2">
+        <label for="" class="col-form-label col-form-label-sm">#5</label>
+        <input name="zwr5" type="text" class="form-control form-control-sm" value="7/8cd">
+      </div>
+      <div class="form-group col-sm-2">
+        <label for="" class="col-form-label col-form-label-sm">#6</label>
+        <input name="zwr6" type="text" class="form-control form-control-sm" value="8ab">
+      </div>
+      <div class="form-group col-sm-2">
+        <label for="" class="col-form-label col-form-label-sm">#7</label>
+        <input name="zwr7" type="text" class="form-control form-control-sm" value="9">
+      </div>
+      <div class="form-group col-sm-2">
+        <label for="" class="col-form-label col-form-label-sm">#8</label>
+        <input name="zwr8" type="text" class="form-control form-control-sm" value="10">
+      </div>
+      <div class="form-group col-sm-2">
+        <label for="" class="col-form-label col-form-label-sm">#9</label>
+        <input name="zwr9" type="text" class="form-control form-control-sm" value="Wk4">
+      </div>
+      <div class="form-group col-sm-2">
+        <label for="" class="col-form-label col-form-label-sm">#10</label>
+        <input name="zwr10" type="text" class="form-control form-control-sm" value="14">
+      </div>
+    </div>
+
+
+
+
+    <button type="submit" class="btn btn-primary">Aktualizuj</button>
+    
+  </form>
+  <button type="" class="btn btn-primary x">Usun</button>
 	</div><!-- col-4 -->
 </div><!-- row -->
 
